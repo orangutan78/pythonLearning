60b5cbd5f5a9ae503275727dfdf172ebca742a89
orangutan78 <orangutan78@qq.com>
2021-10-07 16:42:12 +0800

    107

--- 周逸尘/python/未完成/正在做/魔性点击.py
+++
@@ -1,8 +0,0 @@
-from pymouse import PyMouse
-from random import*
-from time import sleep
-pms=PyMouse()
-sleep(1)
-for i in range(50):
-    pms.click(randint(0,1960),randint(0,1280))
-    sleep(0.1)
